695facfc8aea1258f77da677816feb46209f9e59
guoyanxia <2537158536@qq.com>
2018-09-18 21:36:04 +0800

    init commit

---
+++ 05-timer/01-delay-call.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function Bomb(){
+  this.message="bomb";
+}
+var bomb=new Bomb();
+Bomb.prototype.explode=function(){
+  console.log(this.message);
+}
+var timeId=setTimeout(bomb.explode.bind(bomb),2000)
+ // clearTimeout(timeId);
+
+  
+  
+  
+
---
+++ 05-timer/02-timer.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+console.log("start-----");
+var count=0;
+var timeId=setInterval(loop,500);
+//重复执行任务次数达到上限后，停止执行
+function loop(){
+  console.log("我要一直执行");
+  if((count++)==10){
+    global.clearInterval(timeId);
+  }
+}
+//执行一定时间后停止执行
+global.setTimeout(()=>{
+  console.log("end");
+  global.clearInterval(timeId);
+},500)
+
+
+
+
+
+
+
